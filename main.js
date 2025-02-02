86864ba5eccac892acc8cbeb8e6bdfc0c7e79288
Anuj Kr. Jha <96413803+its-jhaanuj-21@users.noreply.github.com>
2025-02-02 20:36:51 +0530

    unminify js

--- main.js
+++ main.js
@@ -1,1 +1,79 @@
-const navMenu=document.getElementById("nav-menu"),navToggle=document.getElementById("nav-toggle"),navClose=document.getElementById("nav-close");navToggle&&navToggle.addEventListener("click",()=>{navMenu.classList.add("show-menu")}),navClose&&navClose.addEventListener("click",()=>{navMenu.classList.remove("show-menu")});const navLink=document.querySelectorAll(".nav__link"),linkAction=()=>{let e=document.getElementById("nav-menu");e.classList.remove("show-menu")};navLink.forEach(e=>e.addEventListener("click",linkAction));const blurHeader=()=>{let e=document.getElementById("header");this.scrollY>=50?e.classList.add("blur-header"):e.classList.remove("blur-header")};window.addEventListener("scroll",blurHeader);const scrollUp=()=>{let e=document.getElementById("scroll-up");this.scrollY>=350?e.classList.add("show-scroll"):e.classList.remove("show-scroll")},sections=document.querySelectorAll("section[id]"),scrollActive=()=>{let e=window.scrollY;sections.forEach(l=>{let s=l.offsetHeight,t=l.offsetTop-58,n=l.getAttribute("id"),o=document.querySelector(".nav__menu a[href*="+n+"]");e>t&&e<=t+s?o.classList.add("active-link"):o.classList.remove("active-link")})};window.addEventListener("scroll",scrollActive);
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+/* Menu show */
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/* Menu hidden */
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link')
+
+const linkAction = () =>{
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*=============== ADD BLUR TO HEADER ===============*/
+const blurHeader = () =>{
+    const header = document.getElementById('header')
+    this.scrollY >= 50 ? header.classList.add('blur-header') 
+                       : header.classList.remove('blur-header')
+}
+window.addEventListener('scroll', blurHeader)
+
+/*=============== SHOW SCROLL UP ===============*/ 
+const scrollUp = () =>{
+	const scrollUp = document.getElementById('scroll-up')
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+						: scrollUp.classList.remove('show-scroll')
+}
+
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+    
+const scrollActive = () =>{
+  	const scrollDown = window.scrollY
+
+	sections.forEach(current =>{
+		const sectionHeight = current.offsetHeight,
+			  sectionTop = current.offsetTop - 58,
+			  sectionId = current.getAttribute('id'),
+			  sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+		if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+			sectionsClass.classList.add('active-link')
+		}else{
+			sectionsClass.classList.remove('active-link')
+		}                                                    
+	})
+}
+window.addEventListener('scroll', scrollActive)
+
+
+/*=============== LOCK RIGHT CLICK - PREVENT INSPECT =============*/
+// document.addEventListener('contextmenu', function(e) {
+//     e.preventDefault();
+//   });
+
+
+//   document.addEventListener('keydown', function(e) {
+//     if (e.key === 'F12' || (e.ctrlKey && e.shiftKey && e.key === 'I') || (e.ctrlKey && e.shiftKey && e.key === 'J') || (e.ctrlKey && e.key === 'U')) {
+//       e.preventDefault();
+//     }
+//   });
