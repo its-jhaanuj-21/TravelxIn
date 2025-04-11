93b1091a141e32d227a4b430056baa7bfd24cff3
Anuj Kr. Jha <96413803+its-jhaanuj-21@users.noreply.github.com>
2025-04-11 16:19:02 +0530

    updated service worker

--- service-worker.js
+++ service-worker.js
@@ -85,16 +85,16 @@ self.addEventListener('fetch', (event) => {
 });
 
 // Notification click - refresh tab or open new one
-self.addEventListener('notificationclick', (event) => {
-    event.notification.close();
-    event.waitUntil(
-        clients.matchAll({ type: 'window' }).then((clientList) => {
-            for (const client of clientList) {
-                if ('focus' in client) return client.focus();
-            }
-            if (clients.openWindow) {
-                return clients.openWindow('/');
-            }
-        })
-    );
-});
+// self.addEventListener('notificationclick', (event) => {
+//     event.notification.close();
+//     event.waitUntil(
+//         clients.matchAll({ type: 'window' }).then((clientList) => {
+//             for (const client of clientList) {
+//                 if ('focus' in client) return client.focus();
+//             }
+//             if (clients.openWindow) {
+//                 return clients.openWindow('/');
+//             }
+//         })
+//     );
+// });
